daa69f828dafceb01f4cfcc0858bbc61ef1e7d6c
Wales <0189777747@HQ.DHS.GOV>
2016-02-01 23:58:12 -0500

    Begin working on game extractor

--- fetchTournaments.R
+++ fetchTournaments.R
@@ -157,6 +157,102 @@ save(tournamentRosters, file="data/tournamentRosters.Rda")
 load("data/tournamentRosters.Rda")
 
 
+extractGameList <- function(league = "all", rawTournList = leagueTournamentList){
+  
+  suppressMessages(suppressWarnings(library(jsonlite)))
+  suppressMessages(suppressWarnings(library(dplyr)))
+  
+  ## set up vector of leagues
+  leagues = c(1,2,3,4,5,6,7,8,9,12,14,17,18)
+  leagueNames <- c("all-star",
+                   "na-lcs",
+                   "eu-lcs",
+                   "na-cs",
+                   "eu-cs",
+                   "lck",
+                   "lpl",
+                   "lms",
+                   "world-championship",
+                   "iwc",
+                   "cblol",
+                   "copa-south",
+                   "copa-north")
+  names(leagues) <- leagueNames
+  
+  ## Populate league if "all"
+  if (any(league == "all")) {
+    league <- c("all-star",
+                "na-lcs",
+                "eu-lcs",
+                "na-cs",
+                "eu-cs",
+                "lck",
+                "lpl",
+                "lms",
+                "world-championship",
+                "iwc",
+                "cblol",
+                "copa-south",
+                "copa-north")
+  }
+  
+  ## Test if league exists
+  if (any(!(league %in% leagueNames))) {
+    stop("League does not exist.")
+  }
+  
+  ## Initialize df to hold games
+  tournamentGames <- data.frame(tournamentID = character(),
+                                tournamentName = character(),
+                                tournamentDescription = character(),
+                                tournamentLeague = numeric(),
+                                tournamentStartDate = character(),
+                                tournamentEndDate = character(),
+                                bracketID = character(),
+                                matchID = character(),
+                                gameName = character(),
+                                gameID = character(),
+                                gameRealm = character(),
+                                gameCode = numeric(),
+                                stringsAsFactors = FALSE)
+  
+  ## Begin looping through each league
+  for (i in 1:length(league)) {
+    
+    ## Begin looping through each tournament
+    for (j in 1:length(rawTournList[[league[i]]][["highlanderTournaments"]][["id"]])) {
+      
+      ## Store tournament information
+      tournamentID <- rawTournList[[league[i]]][["highlanderTournaments"]][[j, "id"]]
+      tournamentName <- rawTournList[[league[i]]][["highlanderTournaments"]][[j, "title"]]
+      tournamentDescription <- rawTournList[[league[i]]][["highlanderTournaments"]][[j, "description"]]
+      tournamentLeague <- rawTournList[[league[i]]][["highlanderTournaments"]][[j, "league"]]
+      
+      ## Test if start and end dates are included
+      if ("startDate" %in% names(rawTournList[[i]][["highlanderTournaments"]])) {
+        
+        tournamentStartDate = rawTournList[[league[i]]][["highlanderTournaments"]][[j, "startDate"]]
+        tournamentEndDate = rawTournList[[league[i]]][["highlanderTournaments"]][[j, "endDate"]]
+        
+      }
+      
+      ## Extract the tournament brackets
+      bracketsList <- as.list(rawTournList[[league[i]]][["highlanderTournaments"]]["brackets"])
+      
+    }
+    
+  }
+  
+}
+
+for (i in 1:length(rawTournList)) {
+  
+  print(paste0(names(rawTournList)[i], " - ",
+         length(rawTournList[[i]][["highlanderTournaments"]][["id"]]), " - ",
+         length(rawTournList[[i]][["highlanderTournaments"]][["brackets"]])))
+  
+}
+
 
      
 ##NOTES  
